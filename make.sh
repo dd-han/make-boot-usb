971bc9f1fb779313b9362abdf0c7ea00efdc6c5f
dd-han <han@dd-han.tw>
2018-04-18 15:43:43 +0800

    add syslinux support for PXEBoot Enviroment

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ENABLE_GRUB="TRUE"
+ENABLE_SYSLINUX="TRUE"
 
 declare -A UBUNTU_RELEASE
 UBUNTU_RELEASE=( ["trusty-updates"]="14.04 LTS (Trusty Tahr)" ["xenial-updates"]="16.04 LTS (Xenial Xerus)" ["artful-updates"]="17.10 (Artful Aradvark)" ["bionic"]="18.04 LTS (Bionic Beaver)")
@@ -7,13 +9,21 @@ declare -A DEBIAN_RELEASE
 DEBIAN_RELEASE=( ["wheezy"]="7 (wheezy)" ["jessie"]="8 (jessie)" ["stretch"]="9 (stretch)" ["buster"]="10 (buster)")
 
 ####
-## install grub
-mkdir root/EFI
-sudo grub-install --target=i386-pc --boot-directory=root/EFI $1
-sudo grub-install --target=i386-efi --removable --boot-directory=root/EFI --efi-directory=root --grub-mkdevicemap=root/EFI/grub/device.map $1
-sudo grub-install --target=x86_64-efi --removable --boot-directory=root/EFI --efi-directory=root --grub-mkdevicemap=root/EFI/grub/device.map $1
-cp grub.cfg root/EFI/grub
-touch root/EFI/DD-HAN-BOOT-TAG.txt
+if [ "$ENABLE_GRUB" == "TRUE" ]; then 
+    mkdir root/EFI
+    touch root/EFI/DD-HAN-BOOT-TAG.txt
+    sudo grub-install --target=i386-pc --boot-directory=root/EFI $1
+    sudo grub-install --target=i386-efi --removable --boot-directory=root/EFI --efi-directory=root --grub-mkdevicemap=root/EFI/grub/device.map $1
+    sudo grub-install --target=x86_64-efi --removable --boot-directory=root/EFI --efi-directory=root --grub-mkdevicemap=root/EFI/grub/device.map $1
+    cp grub.cfg root/EFI/grub
+fi
+
+if [ "$ENABLE_GRUB" == "TRUE" ]; then 
+    cp syslinux.cfg root/
+    sudo extlinux --install root
+    cp /usr/lib/syslinux/bios/*.c32 root
+    sudo dd bs=440 conv=notrunc count=1 if=/usr/lib/syslinux/bios/mbr.bin of=$1
+fi
 
 
 cd "source"
@@ -78,12 +88,25 @@ for code in "${!UBUNTU_RELEASE[@]}"; do
     sed -i "s/\/linux/\/lives\/ubuntu-$code\/amd64\/linux/g" $code-amd64/boot/grub/grub.cfg
     sed -i "s/\/initrd.gz/\/lives\/ubuntu-$code\/amd64\/initrd.gz/g" $code-amd64/boot/grub/grub.cfg
 
+    for cfg in `ls $code-amd64/*.cfg`;do
+        sed -i "s/[^ ]*\.cfg/lives\/ubuntu-$code\/amd64\/&/g" "$cfg"
+        sed -i "s/[^ ]*\.png/lives\/ubuntu-$code\/amd64\/&/g" "$cfg"
+        sed -i "s/kernel linux/kernel lives\/ubuntu-$code\/amd64\/linux/g" "$cfg"
+        sed -i "s/initrd.gz/lives\/ubuntu-$code\/amd64\/&/g" "$cfg"
+    done
+
     rm -rf $code-amd64/boot/grub/x86_64-efi
     rm -rf $code-amd64/boot/grub/efi.img
     rm -rf $code-amd64/\[BOOT\]
 
     cp -r $code-amd64/boot $code-i386/boot
     sed -i "s/$code\/amd64/$code\/i386/g" $code-i386/boot/grub/grub.cfg
+    for cfg in `ls $code-i386/*.cfg`;do
+        sed -i "s/[^ ]*\.cfg/lives\/ubuntu-$code\/i386\/&/g" "$cfg"
+        sed -i "s/[^ ]*\.png/lives\/ubuntu-$code\/i386\/&/g" "$cfg"
+        sed -i "s/kernel linux/kernel lives\/ubuntu-$code\/i386\/linux/g" "$cfg"
+        sed -i "s/initrd.gz/lives\/ubuntu-$code\/i386\/&/g" "$cfg"
+    done
     rm -rf $code-i386/\[BOOT\]
 done
 
@@ -97,10 +120,13 @@ for code in "${!UBUNTU_RELEASE[@]}"; do
     cp $code-amd64/linux ../root/lives/ubuntu-$code/amd64
     cp $code-amd64/initrd.gz ../root/lives/ubuntu-$code/amd64
     cp -r $code-amd64/boot ../root/lives/ubuntu-$code/amd64
+    cp -r $code-amd64/*.{cfg,txt,png} ../root/lives/ubuntu-$code/amd64
 
     cp $code-i386/linux ../root/lives/ubuntu-$code/i386
     cp $code-i386/initrd.gz ../root/lives/ubuntu-$code/i386
     cp -r $code-i386/boot ../root/lives/ubuntu-$code/i386
+    cp -r $code-i386/*.cfg ../root/lives/ubuntu-$code/i386
+    cp -r $code-i386/*.txt ../root/lives/ubuntu-$code/i386
 
     echo "menuentry \"Ubuntu ${UBUNTU_RELEASE[$code]} 32Bit\" {" >> ../root/EFI/grub/grub.cfg
     echo "	configfile /lives/ubuntu-$code/i386/boot/grub/grub.cfg" >> ../root/EFI/grub/grub.cfg
@@ -109,6 +135,15 @@ for code in "${!UBUNTU_RELEASE[@]}"; do
     echo "menuentry \"Ubuntu ${UBUNTU_RELEASE[$code]} 64Bit\" {" >> ../root/EFI/grub/grub.cfg
     echo "    configfile /lives/ubuntu-$code/amd64/boot/grub/grub.cfg" >> ../root/EFI/grub/grub.cfg
     echo "}" >> ../root/EFI/grub/grub.cfg
+
+    ## syslinux CONFIG
+    echo "LABEL Ubuntu ${UBUNTU_RELEASE[$code]} 32Bit" >> ../root/syslinux.cfg
+    echo "    KERNEL vesamenu.c32" >> ../root/syslinux.cfg
+    echo "    APPEND lives/ubuntu-$code/i386/menu.cfg" >> ../root/syslinux.cfg
+
+    echo "LABEL Ubuntu ${UBUNTU_RELEASE[$code]} 64Bit" >> ../root/syslinux.cfg
+    echo "    KERNEL vesamenu.c32" >> ../root/syslinux.cfg
+    echo "    APPEND lives/ubuntu-$code/amd64/menu.cfg" >> ../root/syslinux.cfg
 done
 
 
